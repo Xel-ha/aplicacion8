835ab92cf85e1f9a4562514625c5da6b06f9e576
Xel-ha <xelha.dsaupoo@gmail.com>
2016-05-11 08:57:32 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -16,7 +16,7 @@ $('#derecha').on("swiperight",function(){
 			break;
 			
 			case 2:
-			navigator.notification.vibrater(1000);
+			navigator.notification.vibrate(1000);
 			break;
 		}
 	},"Aplicacion8","Beep,Vibrar,Cancelar");
